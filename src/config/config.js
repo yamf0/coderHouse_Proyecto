030659f5cd1545d5ac10ef2a217b2422e64df5fe
Yael Martinez <13.ymartinez.2b@gmail.com>
2022-06-30 20:20:02 -0500

    chabnged mongo pwd name

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 import "dotenv/config"
 
-const pwd = encodeURIComponent(process.env.PWD)
+const pwd = encodeURIComponent(process.env.MONGOPWD)
   
 const mongoURL  ='mongodb+srv://admin:' + pwd + '@cluster0.ii4wa.mongodb.net/ecommerce'
 
